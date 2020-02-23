724949f475f75b77ed336b69caeba5766e106e05
wedexyz <38834987+wedexyz@users.noreply.github.com>
2020-02-23 17:02:09 +0700

    Add files via upload

---
+++ pir.ino
@@ -0,0 +1,30 @@
+#define PIR_PIN 2
+#define LED_PIN 13
+long int
+ledOn =1000,
+lastTime = -ledOn;
+void setup(){
+ Serial.begin(9600);
+ pinMode(LED_PIN, OUTPUT); // Atur pin 13 menjadi mode Output
+ pinMode(PIR_PIN, INPUT); // Atur pin 2 menjadi mode input
+ 
+ digitalWrite(LED_PIN, LOW); // matikan led indikator
+ 
+ delay(100); // Jeda sebentar
+}
+ 
+void loop(){
+ if(digitalRead(PIR_PIN) == HIGH){
+  lastTime = millis();
+  Serial.println("Motion Detected");
+  delay(100);
+ }else{
+  Serial.println("No Motion");
+  delay(100);
+ }
+ 
+ if ((millis() - lastTime) < ledOn)
+  digitalWrite(LED_PIN, HIGH);
+ else
+  digitalWrite(LED_PIN, LOW);
+}
